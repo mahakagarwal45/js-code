f1a672add6bae8957231e3eba4dfe0213f218f7e
Mahak Agarwal <mahakagarwal45@gmail.com>
2024-07-25 12:01:21 +0530

    version1.0

---
+++ createobject.js
@@ -0,0 +1,7 @@
+const student_info = {
+    name: "mahak",
+    age: 19
+}
+console.log(student_info);
+console.log(student_info.name);
+console.log(student_info.age);
---
+++ swapvalues.js
@@ -0,0 +1,18 @@
+//Swap the values of a and b using a third variable
+// let a = 5;
+// let b = 9;
+// console.log("a, b = ", a , b);
+// let temp = a;
+// a = b;
+// b = temp;
+// console.log("a, b = ", a, b);
+
+//Swap the values of a and b without using a third variable
+let a = 5;
+let b = 9;
+console.log("a, b = ", a , b);
+a = a+b;
+b = a-b;
+a = a-b;
+console.log("a, b = ", a, b);
+
